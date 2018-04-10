527aa6a433adfcd621e6514fc0f5df5a088e3e7b
Tarun Khubchandani <tarun@kfit.com>
2018-04-10 13:19:16 -0400

    Created student starter code

--- app.py
+++ app.py
@@ -20,53 +20,42 @@ tasks = [
 
 @app.route('/')
 def index():
-    return redirect(url_for("all_tasks"))
+    # TODO: Redirect to all_tasks
+    return
 
 @app.route('/create')
 def create():
-    return render_template("create.html")
+    # TODO: Redirect to form to create a task
+    return
 
 @app.route('/api/v1/tasks')
 def all_tasks():
-    return render_template("alltasks.html", parent_dict=tasks)
+    # TODO: Retrieve all tasks
+    return
 
 @app.route('/api/v1/tasks/<id>', methods=['GET'])
 def get_task(id):
-    id = int(id)
-    task = [task for task in tasks if task['id'] == id]
-    return render_template("task.html", task=task[0])
+    return
 
 @app.route('/api/v1/create', methods=['GET','POST'])
 def create_task():
-    task = {
-            'id': tasks[-1]['id'] + 1,
-            'title': request.form.get("title"),
-            'Priority': request.form.get("Priority"),
-            'completed': request.form.get("completed")
-            }
-    tasks.append(task)
-    return render_template("alltasks.html", parent_dict=tasks)
+    # TODO: Create event and append to dictionary
+    return
 
 @app.route('/api/v1/tasks/<id>', methods=['DELETE'])
 def delete_task(id):
-    id = int(id)
-    task = [task for task in tasks if task['id'] == id]
-    tasks.remove(task[0])
-    return render_template("alltasks.html", parent_dict=tasks)
+    # TODO: Remove a task with the specified ID
+    return
 
 @app.route('/api/v1/tasks/<id>', methods=['PUT'])
 def update_task(id):
-    id = int(id)
-    task = [task for task in tasks if task['id'] == id]
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['Priority'] = request.json.get('Priority', task[0]['Priority'])
-    task[0]['completed'] = request.json.get('completed', task[0]['completed'])
-    return jsonify({'task': task[0]})
+    # TODO: Update a task with the specified ID
+    return
 
 @app.route('/api/v1/tasks/<id>', methods=['POST'])
 def mark_complete(id):
-    id = int(id)
-    return delete_task(id)
+    # TODO: Wrapper for DELETE
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
